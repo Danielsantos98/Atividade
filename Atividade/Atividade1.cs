1052d1db62e85e63ea8132e2f09c24220330f6a2
user <user@DESKTOP-ESCJG7O>
2023-07-01 00:22:08 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade/Atividade1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade
+{
+    internal class Atividade1
+    {
+        public static void Main1q()
+        {
+            /* Questao 1 
+            Console.WriteLine("Qual é o seu nome?");
+            string nome = Console.ReadLine();
+
+            string nomeMaiusculo = nome.ToUpper();
+
+            Console.WriteLine("Qual a sua idade?");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Em qual bairro você mora");
+            string bairro = Console.ReadLine();
+
+            Console.WriteLine($"{nome.ToUpper()}, {idade} anos, moro no {bairro}"); */
+        }
+
+        public static void Main2q()
+        {
+            /* questao 2 
+            Console.WriteLine("Digite um numero inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero > 10)
+            {
+                Console.WriteLine("O numero é positivo.");
+            }
+            else if (numero < 9)
+            {
+                Console.WriteLine("O numero é negativo.");
+            }*/
+        }
+        public static void Main4q()
+        {
+            /*
+             Console.Write("Digite a primeira nota do aluno ");
+             double nota1 = double.Parse(Console.ReadLine());
+
+             Console.Write("Digite a segunda nota do aluno");
+             double nota2 = double.Parse(Console.ReadLine());
+
+             double media = (nota1 + (2 * nota2)) ;
+
+             if (media >= 6.0)
+             {
+                 Console.WriteLine("Aprovado");
+             }
+             else
+             {
+                 Console.WriteLine("Reprovado");
+             }*/
+        }
+        public static void Main5q()
+        {/*
+            Console.WriteLine("Digite sua idade ");
+
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 18 && idade <= 60)
+            {
+                Console.WriteLine("Pode dirigir");
+            }
+            else
+            {
+                Console.WriteLine("Não pode dirigir");
+            } */
+        }  
+        
+    }
+}
